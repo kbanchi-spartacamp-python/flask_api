bd960638d4cdb763b500d188c4d01966e7f26451
kbanchi1111 <kbanchi.1111@gmail.com>
2022-11-12 10:08:57 +0900

    add api

---
+++ app.py
@@ -0,0 +1,11 @@
+import requests
+
+res = requests.get("https://zipcloud.ibsnet.co.jp/api/search?zipcode=7830060")
+
+# print(res.text["status"])
+json = res.json()
+result01 = json["results"][0]
+print(result01["address1"])
+print(result01["address2"])
+print(result01["address3"])
+print(result01["prefcode"])
